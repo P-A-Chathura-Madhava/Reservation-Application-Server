59b4127fc841ab5a754c754d01f42863ef7d0e52
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-12-06 15:19:50 +0530

    Apply auth middleware function

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { createUser, deleteaUser, getAllUsers, getaUser, loginUserCtrl, updateaUser } from '../controller/userController.js';
+import { authMiddleware } from '../config/authMiddleware.js';
 
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
 router.get("/all-users", getAllUsers);
-router.get("/:id", getaUser);
+router.get("/:id", authMiddleware, getaUser);
 router.delete("/:id", deleteaUser);
 router.put("/:id", updateaUser);
 
