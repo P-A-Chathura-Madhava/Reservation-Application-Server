f32ae640140abc6a1c4940432f3ec8762b18bc9f
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-12-08 09:46:36 +0530

    Apply train model

---
+++ models/trainModel.js
@@ -0,0 +1,34 @@
+import { Timestamp } from "mongodb";
+import mongoose from "mongoose";
+
+// Declare the Schema of the Mongo model
+var trainSchema = new mongoose.Schema(
+  {
+    number: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    route: {
+      type: String,
+      required: true,
+    },
+    class: {
+      type: String,
+      enum: ["A", "B", "C", "E"],
+    },
+    seats: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+//Export the model
+const Train = mongoose.model("Train", trainSchema);
+
+export default Train;
