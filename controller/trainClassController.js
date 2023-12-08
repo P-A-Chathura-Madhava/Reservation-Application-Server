3157587598e6f3c8fb83b9ca0539a8bc33418b32
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-12-08 12:26:59 +0530

    Apply get all train classes feature

--- controller/trainClassController.js
+++ controller/trainClassController.js
@@ -29,4 +29,23 @@ const updateATrainClass = asyncHandler(async (req, res) => {
     }
 })
 
-export {createTrainClass, updateATrainClass}
+const getATrainClass = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+      const findTrainClass = await TrainClass.findById(id);
+      res.json(findTrainClass);
+    } catch (error) {
+      throw new Error(error);
+    }
+  });
+
+  const getAllTrainClasses = asyncHandler(async (req, res) => {
+    try {
+        const getTrainClasses = await TrainClass.find();
+        res.json(getTrainClasses);
+      } catch (error) {
+        throw new Error(error);
+      }
+  });
+
+export {createTrainClass, updateATrainClass, getATrainClass, getAllTrainClasses}
--- routes/trainClassRoute.js
+++ routes/trainClassRoute.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createTrainClass, updateATrainClass } from '../controller/trainClassController.js';
+import { createTrainClass, getATrainClass, getAllTrainClasses, updateATrainClass } from '../controller/trainClassController.js';
 
 const router = express.Router();
 
 router.post("/", createTrainClass);
 router.put("/:id", updateATrainClass);
+router.get("/:id", getATrainClass);
+router.get("/", getAllTrainClasses);
 
 export default router;
